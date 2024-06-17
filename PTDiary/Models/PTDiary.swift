9d61c519ca5ab96b9706cb7ec0f0ff67fa0aa018
frankwon11 <frankwon11@gmail.com>
2024-06-17 15:50:46 +0900

    ✨ Feat: PTDiary 모델 정의 (#1)

--- PTDiary/Models/PTDiary.swift
+++ PTDiary/Models/PTDiary.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct PTDiary {
+    /// 제목; PT 13회차 운동 일지
+    var title: String
+    
+    /// 회차; 13
+    var round: Int
+    
+    /// 날짜; 2024.06.10
+    var date: Date
+    
+    /// 오늘 배운 운동들
+    var exercises: [String]
+    
+    /// 새로 배운 점
+    var learned: String?
+    
+    /// 개선해야 할 점
+    var improvements: String?
+    
+    /// 꼭 기억해야 할 점
+    var keyPoints: String?
+    
+    /// 느낀 점
+    var impression: String?
+}
--- PTDiary/PTDiaryApp.swift
+++ PTDiary/PTDiaryApp.swift
@@ -25,7 +25,7 @@ struct PTDiaryApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
         .modelContainer(sharedModelContainer)
     }
